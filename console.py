1e2395c85eabf16fa25070c2ac750da383356005
Panepo <whisper.ko@gmail.com>
2024-06-28 11:07:12 +0800

    update function
    update function

--- console.py
+++ console.py
@@ -1,4 +1,7 @@
+import os
 from agent import agent_executor
+
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = 0
 
 while 1:
     print("================================================")
--- tools/math.py
+++ tools/math.py
@@ -2,45 +2,45 @@ from langchain_core.tools import tool
 
 
 @tool
-def multiply(first_int: int, second_int: int) -> int:
+def multiply(first_int: int, second_int: int) -> int | str:
     """
     Multiply two integers together.
     """
 
     if not isinstance(first_int, int):
-        raise TypeError("The first integer must be a number")
+        return "The first integer must be a number"
 
     if not isinstance(second_int, int):
-        raise TypeError("The second integer must be a number")
+        return "The second integer must be a number"
 
     return first_int * second_int
 
 
 @tool
-def add(first_int: int, second_int: int) -> int:
+def add(first_int: int, second_int: int) -> int | str:
     """
     Add two integers.
     """
 
     if not isinstance(first_int, int):
-        raise TypeError("The first integer must be a number")
+        return "The first integer must be a number"
 
     if not isinstance(second_int, int):
-        raise TypeError("The second integer must be a number")
+        return "The second integer must be a number"
 
     return first_int + second_int
 
 
 @tool
-def exponentiate(base: int, exponent: int) -> int:
+def exponentiate(base: int, exponent: int) -> int | str:
     """
     Exponentiate the base to the exponent power.
     """
 
     if not isinstance(base, int):
-        raise TypeError("The base must be a number")
+        return "The base must be a number"
 
     if not isinstance(exponent, int):
-        raise TypeError("The exponent must be a number")
+        return "The exponent must be a number"
 
     return base**exponent
