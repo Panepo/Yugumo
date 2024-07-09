be8e090a71ce83cf29abafe4c0e60a56725f2e8d
Panepo <whisper.ko@gmail.com>
2024-07-09 14:08:03 +0800

    update function
    update function

--- tools/license.py
+++ tools/license.py
@@ -9,10 +9,15 @@ def license_plate_query(
     Query the car information of plate number of `plate_state` state with plate number `plate_number`
     """
 
-    if not isinstance(plate_state, str):
-        return "the state where license plate registered is required for querying"
+    return "Confirm querying for `plate_state` state plate `plate_number`?"
 
-    if not isinstance(plate_number, str):
-        return "license plate number is required for querying"
+@tool
+def license_plate_query_confirm(
+    plate_state: str,
+    plate_number: str,
+) -> str:
+    """
+    User confirm the query the car information of plate number of `plate_state` state with plate number `plate_number`
+    """
 
-    return "This is a stolen car"
+    return "This is a stolen car."
