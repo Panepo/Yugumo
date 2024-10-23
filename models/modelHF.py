5188486aa794f88f35fac208abbe1e0cf616b87e
Panepo <whisper.ko@gmail.com>
2024-10-23 13:29:58 +0800

    update function
    update function

--- models/modelHF.py
+++ models/modelHF.py
@@ -1,8 +1,4 @@
-from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
-from transformers.generation.stopping_criteria import (
-    StoppingCriteriaList,
-    StoppingCriteria,
-)
+from transformers import AutoModelForCausalLM, AutoTokenizer
 from dotenv import load_dotenv
 import os
 
@@ -19,40 +15,5 @@ else:
 stop_tokens = ["Observation:"]
 model_path = "./models/hfmodels/Mistral-7B-Instruct-v0.3/"
 
-class StopSequenceCriteria(StoppingCriteria):
-    """
-    This class can be used to stop generation whenever a sequence of tokens is encountered.
-
-    Args:
-        stop_sequences (`str` or `List[str]`):
-            The sequence (or list of sequences) on which to stop execution.
-        tokenizer:
-            The tokenizer used to decode the model outputs.
-    """
-
-    def __init__(self, stop_sequences, tokenizer):
-        if isinstance(stop_sequences, str):
-            stop_sequences = [stop_sequences]
-        self.stop_sequences = stop_sequences
-        self.tokenizer = tokenizer
-
-    def __call__(self, input_ids, scores, **kwargs) -> bool:
-        decoded_output = self.tokenizer.decode(input_ids.tolist()[0])
-        return any(
-            decoded_output.endswith(stop_sequence)
-            for stop_sequence in self.stop_sequences
-        )
-
-
-hf_llm = HuggingFacePipeline.from_model_id(
-  model_id=model_path,
-  task="text-generation",
-  device_map=device,
-  pipeline_kwargs={"max_new_tokens": 2048},
-)
-hf_llm = hf_llm.bind(skip_prompt=True, stop=["Observation:"])
-
-tokenizer = hf_llm.pipeline.tokenizer
-hf_llm.pipeline._forward_params["stopping_criteria"] = StoppingCriteriaList(
-    [StopSequenceCriteria(stop_tokens, tokenizer)]
-)
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+model = AutoModelForCausalLM.from_pretrained(model_path)
