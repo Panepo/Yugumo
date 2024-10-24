4290987b0c1d8782ebb5b4a64e1d53fa1b250ec2
Panepo <whisper.ko@gmail.com>
2024-10-24 10:36:36 +0800

    update function
    update function

--- models/modelHF.py
+++ models/modelHF.py
@@ -1,3 +1,4 @@
+from langchain_core.language_models import BaseLanguageModel
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from dotenv import load_dotenv
 import os
@@ -16,4 +17,16 @@ stop_tokens = ["Observation:"]
 model_path = "./models/hfmodels/Mistral-7B-Instruct-v0.3/"
 
 tokenizer = AutoTokenizer.from_pretrained(model_path)
-hf_llm = AutoModelForCausalLM.from_pretrained(model_path)
+model = AutoModelForCausalLM.from_pretrained(model_path)
+
+class LLM(BaseLanguageModel):
+    def __init__(self, model, tokenizer):
+        self.model = model
+        self.tokenizer = tokenizer
+
+    def generate(self, prompt):
+        inputs = self.tokenizer(prompt, return_tensors="pt")
+        outputs = self.model.generate(**inputs)
+        return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+hf_llm = LLM(model, tokenizer)
